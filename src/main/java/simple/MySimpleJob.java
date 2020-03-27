ec506d37dcf6310a37542473f2553a43ee5254c2
shent <shent@163.com>
2020-03-27 14:44:08 +0800

    提交

---
+++ src/main/java/simple/MySimpleJob.java
@@ -0,0 +1,15 @@
+package simple;
+
+import com.dangdang.ddframe.job.api.ShardingContext;
+import com.dangdang.ddframe.job.api.simple.SimpleJob;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MySimpleJob implements SimpleJob {
+    public void execute(ShardingContext context) {
+        System.out.println(String.format("分片项 ShardingItem: %s | 运行时间: %s | 线程ID: %s | 分片参数: %s ",
+                context.getShardingItem(), new SimpleDateFormat("HH:mm:ss").format(new Date()),
+                Thread.currentThread().getId(), context.getShardingParameter()));
+    }
+}
